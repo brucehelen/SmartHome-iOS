986bbc3775c6cd3090ce43b6f65f17275b35d2b5
brucehelen <sudazzj@qq.com>
2015-12-07 07:29:53 +0800

    Initial Commit

---
+++ SmartHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartHome
+//
+//  Created by 朱正晶 on 15/12/7.
+//  Copyright © 2015年 china. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartHome
+//
+//  Created by 朱正晶 on 15/12/7.
+//  Copyright © 2015年 china. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
